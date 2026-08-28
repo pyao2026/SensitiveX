cd9ac2077ca8062435d25f84c54c603aa24647b2
pyao2026 <pyao2026@outlook.com>
2026-08-28 13:09:52 +0800

    加入TailwindCSS支持，并修复favicon问题

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     clearScreen: false,
     server: {
         port: 1420,
